789264963dbd163f06cdf157ec3b62cc77938461
Eimantas Morkūnas <eimantas.morkunas@ktu.edu>
2016-02-25 13:23:28 +0200

    Gyro ir Acc app
    App rodantis giroskopo ir akcelerometro parodymus

---
+++ GyroAcc/app/src/main/java/com/example/eimantas/gyroacc/AccActivity.java
@@ -0,0 +1,69 @@
+package com.example.eimantas.gyroacc;
+
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+
+public class AccActivity extends AppCompatActivity {
+
+    private TextView textViewX;
+    private TextView textViewY;
+    private TextView textViewZ;
+    private TextView textViewXMax;
+    private TextView textViewYMax;
+    private TextView textViewZMax;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_acc);
+        textViewX = (TextView)findViewById(R.id.textViewAccX);
+        textViewY = (TextView)findViewById(R.id.textViewAccY);
+        textViewZ = (TextView)findViewById(R.id.textViewAccZ);
+        textViewXMax = (TextView)findViewById(R.id.textViewAccXMax);
+        textViewYMax = (TextView)findViewById(R.id.textViewAccYMax);
+        textViewZMax = (TextView)findViewById(R.id.textViewAccZMax);
+        SensorManager sm = (SensorManager)getSystemService(SENSOR_SERVICE);
+        Sensor acc = sm.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+        sm.registerListener(new SensorEventListener() {
+            @Override
+            public void onSensorChanged(SensorEvent event) {
+                changeText(event.values[0], event.values[1], event.values[2]);
+            }
+
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+            }
+        }, acc, SensorManager.SENSOR_DELAY_FASTEST);
+    }
+
+    protected void changeText(float x, float y, float z) {
+        textViewX.setText(String.format("%2.2f", x));
+        textViewY.setText(String.format("%2.2f", y));
+        textViewZ.setText(String.format("%2.2f", z));
+
+        if (Float.compare(Float.parseFloat(textViewXMax.getText().toString()), x) < 0) {
+            textViewXMax.setText((String.format("%2.2f", x)));
+        }
+        if (Float.compare(Float.parseFloat(textViewYMax.getText().toString()), y) < 0) {
+            textViewYMax.setText((String.format("%2.2f", y)));
+        }
+        if (Float.compare(Float.parseFloat(textViewZMax.getText().toString()), z) < 0) {
+            textViewZMax.setText((String.format("%2.2f", z)));
+        }
+    }
+
+    public void resetMax(View view) {
+        textViewXMax.setText("0.00");
+        textViewYMax.setText("0.00");
+        textViewZMax.setText("0.00");
+    }
+}
---
+++ GyroAcc/app/src/main/java/com/example/eimantas/gyroacc/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.eimantas.gyroacc;
+
+import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        SensorManager sm = (SensorManager)getSystemService(SENSOR_SERVICE);
+        sm.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+    }
+
+    public void openAcc(View view) {
+        Intent intent = new Intent(this, AccActivity.class);
+        startActivity(intent);
+    }
+
+    public void openGyro(View view) {
+        Intent intent = new Intent(this, GyroActivity.class);
+        startActivity(intent);
+    }
+}
