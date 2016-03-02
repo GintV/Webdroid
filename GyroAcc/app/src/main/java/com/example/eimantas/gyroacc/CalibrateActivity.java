b739e3d738170ac698af83d9a4ace2da0dd4e3b3
Gintaras V <gintaras.volkvicius@gmail.com>
2016-03-02 22:26:28 +0200

    Pašalinta gravitacija iš akselerometro

--- GyroAcc/app/src/main/java/com/example/eimantas/gyroacc/CalibrateActivity.java
+++ GyroAcc/app/src/main/java/com/example/eimantas/gyroacc/CalibrateActivity.java
@@ -34,21 +34,44 @@ public class CalibrateActivity extends AppCompatActivity {
             float[] last_values = null;
             float[] velocity = null;
             float[] position = null;
+            float[] gravity = null;
+            float[] real_acc = null;
             long last_timestamp = 0;
+
+            // alpha is calculated as t / (t + dT)
+            // with t, the low-pass filter's time-constant
+            // and dT, the event delivery rate
+
+            final float alpha = 0.8f;
+
 
             @Override
             public void onSensorChanged(SensorEvent event) {
+
                 if (last_values != null) {
                     float dt = (event.timestamp - last_timestamp) * NS2S;
 
+                    gravity[0] = alpha * gravity[0] + (1 - alpha) * event.values[0];
+                    gravity[1] = alpha * gravity[1] + (1 - alpha) * event.values[1];
+                    gravity[2] = alpha * gravity[2] + (1 - alpha) * event.values[2];
+
+                    real_acc[0] = event.values[0] - gravity[0];
+                    real_acc[1] = event.values[1] - gravity[1];
+                    real_acc[2] = event.values[2] - gravity[2];
+
                     for (int index = 0; index < 3; ++index) {
-                        velocity[index] += (event.values[index] + last_values[index]) / 2 * dt;
+                        velocity[index] += (real_acc[index] + last_values[index]) / 2 * dt;
                         position[index] += velocity[index] * dt;
                     }
                 } else {
+                    gravity = new float[3];
+                    real_acc = new float[3];
+
                     last_values = new float[3];
                     velocity = new float[3];
                     position = new float[3];
+
+                    gravity[0] = gravity[1] = gravity[2] = 0f;
                     velocity[0] = velocity[1] = velocity[2] = 0f;
                     position[0] = position[1] = position[2] = 0f;
                 }
