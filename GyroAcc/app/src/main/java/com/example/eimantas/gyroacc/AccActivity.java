ffdb627cd539117fbc7afdf613b74af3050c25ba
Gintaras V <gintaras.volkvicius@gmail.com>
2016-03-02 23:14:02 +0200

    Prie akselerometro pridedamos greičio reikšmės

--- GyroAcc/app/src/main/java/com/example/eimantas/gyroacc/AccActivity.java
+++ GyroAcc/app/src/main/java/com/example/eimantas/gyroacc/AccActivity.java
@@ -19,6 +19,10 @@ public class AccActivity extends AppCompatActivity {
     private TextView textViewYMax;
     private TextView textViewZMax;
 
+    private TextView textSpeedX;
+    private TextView textSpeedY;
+    private TextView textSpeedZ;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,25 +33,33 @@ public class AccActivity extends AppCompatActivity {
         textViewXMax = (TextView)findViewById(R.id.textViewAccXMax);
         textViewYMax = (TextView)findViewById(R.id.textViewAccYMax);
         textViewZMax = (TextView)findViewById(R.id.textViewAccZMax);
+
+        textSpeedX = (TextView) findViewById(R.id. textViewSpeedX);
+        textSpeedY = (TextView) findViewById(R.id. textViewSpeedY);
+        textSpeedZ = (TextView) findViewById(R.id. textViewSpeedZ);
         SensorManager sm = (SensorManager)getSystemService(SENSOR_SERVICE);
         Sensor acc = sm.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
 
 
 
         sm.registerListener(new SensorEventListener() {
+            static final float NS2S = 1.0f / 1000000000.0f;
 
             float[] gravity = null;
+            float[] velocity = null;
+            float[] last_values = null;
             float[] real_acc = null;
+            long last_timestamp = 0;
             final float alpha = 0.8f;
-
-
 
             @Override
             public void onSensorChanged(SensorEvent event) {
-                if (gravity == null) {
+                if (last_values == null) {
                     gravity = new float[3];
                     real_acc = new float[3];
                     gravity[0] = gravity[1] = gravity[2] = 0f;
+                    velocity[0] = velocity[1] = velocity[2] = 0f;
+                    last_values[0] = last_values[1] = last_values[2] = 0f;
                 }
 
                 gravity[0] = alpha * gravity[0] + (1 - alpha) * event.values[0];
@@ -59,6 +71,18 @@ public class AccActivity extends AppCompatActivity {
                 real_acc[2] = event.values[2] - gravity[2];
 
                 changeText(real_acc[0], real_acc[1], real_acc[2]);
+
+                float dt = (event.timestamp - last_timestamp) * NS2S;
+
+                for (int index = 0; index < 3; ++index) {
+                    velocity[index] += (real_acc[index] + last_values[index]) / 2 * dt;
+                }
+
+                changeSpeed(velocity[0], velocity[1], velocity[2]);
+
+                System.arraycopy(real_acc, 0, last_values, 0, 3);
+                last_timestamp = event.timestamp;
+
             }
 
             @Override
@@ -82,6 +106,12 @@ public class AccActivity extends AppCompatActivity {
         if (Float.compare(Float.parseFloat(textViewZMax.getText().toString()), z) < 0) {
             textViewZMax.setText((String.format("%2.2f", z)));
         }
+    }
+
+    protected void changeSpeed(float speedX, float speedY, float speedZ) {
+        textSpeedX.setText(String.format("%2.2f"));
+        textSpeedY.setText(String.format("%2.2f"));
+        textSpeedZ.setText(String.format("%2.2f"));
     }
 
     public void resetMax(View view) {
