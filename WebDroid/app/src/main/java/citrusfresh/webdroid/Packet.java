18055108e9d38a42cc2fe3efd20b3a59d54cd4fa
Gintaras V <gintaras.volkvicius@gmail.com>
2016-05-15 23:40:10 +0300

    WebDroid: prideda Packet klase, aprasyti static kintamieji kurie bus naudojami nustatyt type reiksmem

---
+++ WebDroid/app/src/main/java/citrusfresh/webdroid/Packet.java
@@ -0,0 +1,39 @@
+package citrusfresh.webdroid;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+/**
+ * Created by Gintaras on 2016.05.15.
+ */
+public abstract class Packet {
+    private static final String TYPE_NEW_PLAYER = "newPlayer";
+    private static final String TYPE_PLAYER_INFO_CHANGE = "changePlayer";
+    private static final String TYPE_PLAYER_POSITION = "joystick";
+    private static final String TYPE_PLAYER_READY = "readyPlayer";
+
+    private String type;
+    private Object data;
+
+    public void setType(String type){
+        this.type = type;
+    }
+
+    public void setData(Object data){
+        this.data = data;
+    }
+
+    public String toJSON() {
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        String jsonString = null;
+
+        try {
+            jsonString = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(this);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        return jsonString;
+    }
+
+}
